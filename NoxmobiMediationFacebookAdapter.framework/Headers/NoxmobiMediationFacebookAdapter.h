10b81fe7681e6daec6947bdff5264e86bc134727
nox_chong <gaochong@bignox.com>
2020-07-14 19:50:22 +0800

    v5.10.0.0

---
+++ NoxmobiMediationFacebookAdapter.framework/Headers/NoxmobiMediationFacebookAdapter.h
@@ -0,0 +1,15 @@
+//
+//	NoxmobiMediationFacebookAdapter.h
+//	
+//	▄▄︻┻━━━┳━═━━   BUG~~
+//	
+//  Created by Chong on 2020/7/13.
+//  Copyright © 2020 Chong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+#define NoxmobiMediationFacebookAdapterVersion @"5.10.0.0"
+
+
